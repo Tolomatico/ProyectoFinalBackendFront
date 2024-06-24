bba09873e3fe6bdee644d280cf17cc78d9052965
Tolomatico <ballesty.t@gmail.com>
2024-06-24 00:37:51 -0300

    env variables modified

--- src/components/products/ProductRealTime.jsx
+++ src/components/products/ProductRealTime.jsx
@@ -51,7 +51,7 @@ export function ProductRealTime({ product, fetchProducts }) {
 
     return (
         <article className=" w-60 h-fit justify-center text-center  rounded-lg shadow p-5 m-5">
-            <img className="w-32 h-32 mx-auto" src={`http://localhost:8080/img/${thumbnail}`} />
+            <img className="w-32 h-32 mx-auto" src={`${import.meta.env.VITE_API_URL}/img/${thumbnail}`} />
             <p className="font-bold text-xl">{title}</p>
             <p className="font-bold text-xl">Precio: ${price}</p>
             <p className="font-bold text-xl">stock: {stock}u.</p>
